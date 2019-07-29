fb633adb8dad2aeabeb39b8aa22b91ba79e9b9b1
Zach Wegner <zwegner+atxled@gmail.com>
2019-07-29 17:36:48 -0500

    Make update.sh always update to origin/master

--- update.sh
+++ update.sh
@@ -4,7 +4,10 @@ set -e
 INSTALL_DIR=/home/pi/atxled/hue
 
 echo 'Grabbing updates...'
-git pull
+# Ignore all local changes, oh what fun!
+git reset --hard
+git fetch
+git checkout origin/master
 rm -rf $INSTALL_DIR
 mkdir $INSTALL_DIR
 unzip bundle.zip -d $INSTALL_DIR
