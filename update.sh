f0f152bbcf569befbb047715dbd4818bf3963b31
Zach Wegner <zwegner+atxled@gmail.com>
2019-08-05 17:39:16 -0500

    Allow setting the branch to pull from

--- update.sh
+++ update.sh
@@ -3,11 +3,14 @@ set -e
 
 INSTALL_DIR=/home/pi/atxled/hue
 
+BRANCH='origin/master'
+[ -f branch ] && BRANCH=`cat branch`
+
 echo 'Grabbing updates...'
 # Ignore all local changes, oh what fun!
 git reset --hard
 git fetch
-git checkout origin/master
+git checkout $BRANCH
 rm -rf $INSTALL_DIR
 mkdir $INSTALL_DIR
 unzip bundle.zip -d $INSTALL_DIR
