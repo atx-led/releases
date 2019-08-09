33c77c93be56e23667d8062a2db06d28bed1f0ad
Zach Wegner <zwegner+atxled@gmail.com>
2019-08-09 13:44:57 -0500

    Be sure to enable serial port

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e
 
 echo 'Installing auto-updater...'
@@ -7,4 +8,24 @@ sudo cp ${UNIT} /lib/systemd/system/
 sudo chmod 644 /lib/systemd/system/${UNIT}
 sudo systemctl daemon-reload
 sudo systemctl enable ${UNIT}
-sudo systemctl start ${UNIT}
+
+SERIAL=$(raspi-config nonint get_serial_hw)
+
+# If serial port is enabled already, start the service
+if [ $SERIAL -eq 0 ]; then
+    sudo systemctl start ${UNIT}
+# Otherwise, enable serial and reboot
+else
+    # Enable serial port, but not serial console
+    # The raspi-config script is kinda weird, and isn't documented much. The first arg has to be 2 and not 1
+    # for some reason...
+    echo 'Enabling serial port...'
+    sudo raspi-config nonint do_serial 2 0
+
+    echo ''
+    echo 'To complete installation, the machine must be rebooted.'
+    echo -n 'Reboot now? [y/N] ' && read resp
+    if [ "$resp" = 'y' ] || [ "$resp" = 'yes' ] || [ "$resp" = 'Y' ]; then
+        sudo reboot
+    fi
+fi
