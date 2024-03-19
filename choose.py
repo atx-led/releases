f7d55e99756f1da827f94d1c9d88bbfb287ad505
rijimuge <david.russell04@gmail.com>
2024-03-19 14:41:56 -0500

    update bundles

--- choose.py
+++ choose.py
@@ -5,5 +5,7 @@ import os
 print('Loading for version %s' % (sys.version.split()[0]))
 if sys.version.startswith('3.9'):
     os.system('./load-3.9.bin')
+elif sys.version.startswith('3.11'):
+    os.system('./load-3.11.bin')
 else:
     os.system('./load.bin')
