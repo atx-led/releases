4cc2cc4cbd886529e85bb30fe82be40e2345a817
rijimuge <david.russell04@gmail.com>
2024-03-21 14:57:50 -0500

    update bundles - check for dependency conflict and move it

--- choose.py
+++ choose.py
@@ -2,7 +2,25 @@
 import sys
 import os
 
-print('Loading for version %s' % (sys.version.split()[0]))
+# Define the path to the _cffi_backend file
+cffi_backend_path = '/usr/lib/python3/dist-packages/_cffi_backend.cpython-39-arm-linux-gnueabihf.so'
+
+print('Loading for version %s' % sys.version.split()[0])
+
+def backup_cffi_backend():
+    backup_path = cffi_backend_path + '.backup'
+    if os.path.exists(cffi_backend_path):
+        print('Backing up %s to %s' % (cffi_backend_path, backup_path))
+        # Use sudo to move the file
+        move_command = 'sudo mv ' + cffi_backend_path + ' ' + backup_path
+        os.system(move_command)
+    else:
+        print('%s does not exist, no action taken.' % cffi_backend_path)
+
+# Check and backup _cffi_backend if necessary
+backup_cffi_backend()
+
+# Load different binaries based on Python version
 if sys.version.startswith('3.9'):
     os.system('./load-3.9.bin')
 elif sys.version.startswith('3.11'):
